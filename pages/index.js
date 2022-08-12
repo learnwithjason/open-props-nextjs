599e914eabd7d98385acc618d999170bac352527
Jason Lengstorf <jason@lengstorf.com>
2022-08-12 09:03:57 -0700

    feat: LWJ favicon?!

--- pages/index.js
+++ pages/index.js
@@ -78,7 +78,27 @@ export default function Home({ schedule }) {
           name="description"
           content="A dashboard showing past and upcoming LWJ episodes"
         />
-        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#302d42" />
+        <meta name="msapplication-TileColor" content="#302d42" />
+        <meta name="theme-color" content="#302d42" />
       </Head>
 
       <main className={styles.main}>
